514651908b5b2196d86696a8cf994bac92c9fce5
Matan Kushner <hello@matchai.me>
2018-12-06 17:07:49 -0500

    Add tests for implicitAny

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,49 @@ const execa = require("execa");
 const main = (args, options) =>
   execa(path.join(__dirname, "../cli-main.js"), args, options);
 
+test("the default configuration finds errors", async () => {
+  const cwd = path.join(__dirname, "fixtures/default");
+
+  let error;
+  try {
+    await main(["--reporter=json"], { cwd });
+  } catch (err) {
+    error = err;
+  }
+
+  const diagnostics = JSON.parse(error.stdout);
+  expect(diagnostics).toHaveLength(2);
+  expect(diagnostics[0]).toHaveProperty("file.fileName", "bar.js");
+  expect(diagnostics[0]).toHaveProperty(
+    "messageText",
+    "Cannot find name 'add'."
+  );
+  expect(diagnostics[1]).toHaveProperty("file.fileName", "foo.js");
+  expect(diagnostics[1]).toHaveProperty(
+    "messageText",
+    "Parameter 'n' implicitly has an 'any' type."
+  );
+});
+
+test("implicitAny ignores cases there's no type annotation", async () => {
+  const cwd = path.join(__dirname, "fixtures/default");
+
+  let error;
+  try {
+    await main(["--reporter=json", "--implicitAny"], { cwd });
+  } catch (err) {
+    error = err;
+  }
+
+  const diagnostics = JSON.parse(error.stdout);
+  expect(diagnostics).toHaveLength(1);
+  expect(diagnostics[0]).toHaveProperty("file.fileName", "bar.js");
+  expect(diagnostics[0]).toHaveProperty(
+    "messageText",
+    "Cannot find name 'add'."
+  );
+});
+
 test("ignore files in .gitignore", async () => {
   const cwd = path.join(__dirname, "fixtures/gitignore");
 
