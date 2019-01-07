928fb9c60b72cd51ca087f8fa21a92f82fd17ebf
Matan Kushner <hello@matchai.me>
2019-01-06 21:44:14 -0500

    Add tests for eslint-formatter-adapter

--- test/eslint-formatter-adapter.test.js
+++ test/eslint-formatter-adapter.test.js
@@ -26,3 +26,79 @@ describe("codeToRuleId", () => {
     expect(result).toEqual({ ruleId: "TS7006" });
   });
 });
+
+describe("messageTextToMessage", () => {
+  it("correctly converts a TypeScript messageText to an ESLint report message", () => {
+    const diagnostic = { messageText: "Cannot find name 'add'." };
+    const result = adapter.messageTextToMessage(diagnostic);
+    expect(result).toEqual({ message: "Cannot find name 'add'." });
+  });
+});
+
+describe("addLineAndColumn", () => {
+  it("corectly sets line and column from TypeScript diagnostic", () => {
+    const diagnostic = {
+      message: "Cannot find name 'add'.",
+      file: {
+        text: "add(1, 2);\n",
+        lineMap: [0, 11]
+      },
+      start: 16
+    };
+    const result = adapter.addLineAndColumn(diagnostic);
+    expect(result).toEqual({
+      message: "Cannot find name 'add'.",
+      file: {
+        text: "add(1, 2);\n",
+        lineMap: [0, 11]
+      },
+      line: 1,
+      column: 5
+    });
+  });
+});
+
+describe("removeUnusedKeys", () => {
+  it("correctly deletes the unneeded keys", () => {
+    const diagnostic = {
+      messageText: "Cannot find name 'add'.",
+      length: 1,
+      reportsUnnecessary: null,
+      relatedInformation: "Here is some related information",
+      file: {
+        pos: 0,
+        end: 11,
+        flags: 65536,
+        transformFlags: 536870912,
+        parent: undefined,
+        kind: 279,
+        text: "add(1, 2);\n"
+      }
+    };
+    const result = adapter.removeUnusedKeys(diagnostic);
+    expect(result).toEqual({ messageText: "Cannot find name 'add'." });
+  });
+});
+
+describe("addStatusCount", () => {
+  it("correctly adds status counters", () => {
+    let results = {
+      messages: [{ severity: 1 }, { severity: 1 }, { severity: 2 }]
+    };
+
+    results = adapter.addStatusCount(results, "warning", 1);
+
+    expect(results).toEqual({
+      messages: [{ severity: 1 }, { severity: 1 }, { severity: 2 }],
+      warningCount: 2
+    });
+
+    results = adapter.addStatusCount(results, "error", 2);
+
+    expect(results).toEqual({
+      messages: [{ severity: 1 }, { severity: 1 }, { severity: 2 }],
+      warningCount: 2,
+      errorCount: 1
+    });
+  });
+});
